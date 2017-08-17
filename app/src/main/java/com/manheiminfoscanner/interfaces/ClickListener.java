18f8d42a8659be285bda2d7b54e954c4a0de0071
zfolkz <zfolkz3@gmail.com>
2017-08-18 03:31:28 +0700

    no message

--- app/src/main/java/com/manheiminfoscanner/interfaces/ClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.manheiminfoscanner.interfaces;
-
-/**
- * Created by zfolkz on 1/8/2017 AD.
- */
-
-public interface ClickListener {
-    void ItemClicked(int position);
-}
--- app/src/main/java/com/manheiminfoscanner/model/PhotoFailedData.java
+++
@@ -1,8 +0,0 @@
-package com.manheiminfoscanner.model;
-
-/**
- * Created by zfolkz on 1/14/2017 AD.
- */
-
-public class PhotoFailedData {
-}
